74b1b3163713422f7c5073334623c8a24d50f6a4
EduardoDevFull <eduardodevfull@gmail.com>
2026-08-04 19:57:31 -0300

    feat: redesign home tab with quick-access card grid

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -5,15 +5,41 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
-  IonButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  leafOutline,
+  moonOutline,
+  musicalNotesOutline,
+  waterOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardContent,
+    RouterLink,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ waterOutline, leafOutline, moonOutline, musicalNotesOutline });
+  }
 }
